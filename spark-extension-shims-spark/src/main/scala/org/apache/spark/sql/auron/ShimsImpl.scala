9659655b10d9d782eb5f4aa13a88ddb81b147adf
Ruilei Ma <merrily01@gmail.com>
2025-11-11 17:51:54 +0800

    [AURON #1612] Filter out empty build-info values to improve Spark UI display

--- spark-extension-shims-spark/src/main/scala/org/apache/spark/sql/auron/ShimsImpl.scala
+++ spark-extension-shims-spark/src/main/scala/org/apache/spark/sql/auron/ShimsImpl.scala
@@ -192,6 +192,7 @@ class ShimsImpl extends Shims with Logging {
     auronBuildInfo.put(AuronBuildInfo.PAIMON_VERSION_STRING, AuronBuildInfo.PAIMON_VERSION)
     auronBuildInfo.put(AuronBuildInfo.FLINK_VERSION_STRING, AuronBuildInfo.FLINK_VERSION)
     auronBuildInfo.put(AuronBuildInfo.BUILD_DATE_STRING, AuronBuildInfo.BUILD_DATE)
+    auronBuildInfo.retain { case (_, v) => v != null && v.nonEmpty }
     val event = AuronBuildInfoEvent(auronBuildInfo)
     AuronEventUtils.post(sparkContext, event)
   }
